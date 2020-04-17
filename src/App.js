bd8e73e529eec472efb0cc85fff359b6354ecc00
宮崎貴史 <>
2020-04-17 15:45:14 +0900

    add get links

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  // ダウンロードリンクレンダー
+  const renderDLlinks = (fileId, fileKeys) => {
+    const links = getLinks(fileId, fileKeys)
+    if (links === null) return null
+
+    return links.map((l, key) => {
+      return (
+        <>
+          <a key={key} href={l}>
+            ファイル{key + 1}をダウンロード
+          </a>
+          <br />
+        </>
+      )
+    })
+  }
+
+  // ダウンロードリンクコピー
+  const copyLinkBtn = (fileId, fileKeys) => {
+    const links = getLinks(fileId, fileKeys)
+
+    if (links === null) return
+
+    return <button onClick={() => console.log(links.join('\n'))}>copy</button>
+  }
+
+  const getLinks = (fileId, fileKeys) => {
+    const getUrl = (fileId, fileKey = '') => {
+      return `/download/${fileId}/${fileKey}`
+    }
+
+    if (fileKeys === undefined) return [getUrl(fileId)]
+    if (fileKeys.length === 0) return null
+    if (fileKeys.length === 1) return [getUrl(fileId, fileKeys)]
+    if (fileKeys.length > 1) {
+      return fileKeys.map(k => {
+        return getUrl(fileId, k)
+      })
+    }
+  }
+
+  const fileId = 100
+  const fileKeys = undefined
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {renderDLlinks(fileId, fileKeys)}
+      {copyLinkBtn(fileId, fileKeys)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
